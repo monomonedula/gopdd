e3a0a8b825bcbb89ee21fea132f82727b3536bb6
monomonedula <valh@tuta.io>
2023-03-26 23:27:31 +0300

    resources upd

--- resources/foobar.py
+++ resources/foobar.py
@@ -1,12 +1,12 @@
 
 
     # @todo #209:30m whatever 1234. 
-    #  Please fix soon.
+    #  Please fix soon 1.
     #
 
 def foo():
 
     # @todo #321:1h very important issue.   
-    #  Please fix soon.  
+    #  Please fix soon 2.  
 
     print(1 + 1)
